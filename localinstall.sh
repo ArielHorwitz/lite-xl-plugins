f64a05fca4e9fe661ffb49f3e7231c7d52b7b728
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2025-04-30 21:51:13 +0300

    Move developer tooling

--- localinstall.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-set -e
-
-SOURCE_DIR=$(realpath "$(dirname "$0")")
-DEFAULT_INSTALL_LOCALTION="$XDG_CONFIG_HOME/lite-xl/plugins"
-
-cd "$SOURCE_DIR"
-cp --verbose --force -t "$DEFAULT_INSTALL_LOCALTION" src/*.lua
